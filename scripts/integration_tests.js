a3294d3376e2c5fc0bc77250e4a34d0b66cf35ab
Ayush Menon <ayush@valon.com>
2021-03-12 11:49:04 -0500

    deposit test

--- scripts/integration_tests.js
+++ scripts/integration_tests.js
@@ -1,5 +1,5 @@
-import { LocalTerra } from "@terra-money/terra.js";
-import { deploy, query_contract } from "./helpers.mjs";
+import {Coin, LocalTerra, MsgExecuteContract, MsgSend} from "@terra-money/terra.js";
+import {deploy, perform_transaction, query_contract} from "./helpers.mjs";
 
 async function test_reserve_query(terra, address, symbol) {
   let query_msg = {"reserve": {"symbol": symbol}};
@@ -25,6 +25,26 @@ async function test_config_query(terra, address) {
   console.log(result);
 }
 
+async function test_deposit(terra, wallet, contract_address) {
+  const deposit_msg = {"deposit_native": {"symbol": "luna"}};
+  const coins = new Coin("uluna", 1000);
+  const execute_msg = new MsgExecuteContract(wallet.key.accAddress, contract_address, deposit_msg, [coins]);
+  await perform_transaction(terra, wallet, execute_msg);
+
+  let reserve_query_msg = {"reserve": {"symbol": "luna"}};
+  let { ma_token_address } = await query_contract(terra, contract_address, reserve_query_msg);
+
+  const balance_query_msg = {"balance": {"address": wallet.key.accAddress}};
+  let result = await query_contract(terra, ma_token_address, balance_query_msg);
+
+  if (result.balance === "0") {
+    throw new Error("Config query failed")
+  }
+
+  console.log("Query Result: ");
+  console.log(result);
+}
+
 const terra = new LocalTerra();
 const wallet = terra.wallets.test1;
 const contract_address = await deploy(terra, wallet);
@@ -32,3 +52,4 @@ const contract_address = await deploy(terra, wallet);
 await test_reserve_query(terra, contract_address, "usd")
 await test_reserve_query(terra, contract_address, "luna");
 await test_config_query(terra, contract_address);
+test_deposit(terra, wallet, contract_address);
