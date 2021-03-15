d641f509734cf15b0aecf9aa6bbd73aa8a293e82
Ayush Menon <ayush@valon.com>
2021-03-15 13:46:50 -0400

    added deposit and redeem diff checks

--- scripts/integration_tests.js
+++ scripts/integration_tests.js
@@ -7,15 +7,15 @@ function toEncodedBinary(object) {
 
 async function testReserveQuery(terra, address, symbol) {
   console.log("### Testing Reserve...")
-  let queryMsg = {"reserve": {"symbol": symbol}};
-  let result = await queryContract(terra, address, queryMsg);
+  let reserveQueryMsg = {"reserve": {"symbol": symbol}};
+  let reserveResult = await queryContract(terra, address, reserveQueryMsg);
 
-  if (!result.hasOwnProperty("ma_token_address")) {
+  if (!reserveResult.hasOwnProperty("ma_token_address")) {
     throw new Error(`[Reserve]: Reserve Query for symbol ${symbol} failed. Result has no property ma_token_address.`)
   }
 
-  console.log(`Reserve Query for symbol ${symbol}:`);
-  console.log(result);
+  console.log("Reserve Query Sent:");
+  console.log(reserveQueryMsg);
 }
 
 async function main() {
@@ -35,54 +35,84 @@ async function main() {
     throw new Error("[Config]: Config query failed. Result has no property ma_token_code_id.")
   }
 
-  console.log("Config Query:");
-  console.log(configResult);
+  console.log("Config Query Sent:");
+  console.log(queryMsg);
 
 
-  console.log("### Testing Deposit...")
-  const depositMsg = {"deposit_native": {"symbol": "luna"}};
-  const coins = new Coin("uluna", 1000);
-  const executeDepositMsg = new MsgExecuteContract(wallet.key.accAddress, lpContractAddress, depositMsg, [coins]);
-  await performTransaction(terra, wallet, executeDepositMsg);
+  console.log("### Testing Deposit...");
+  let {_coins: {uluna: {amount: depositorStartingBalance}}} = await terra.bank.balance(wallet.key.accAddress);
 
   let reserveQueryMsg = {"reserve": {"symbol": "luna"}};
   let { ma_token_address } = await queryContract(terra, lpContractAddress, reserveQueryMsg);
-
   const balanceQueryMsg = {"balance": {"address": wallet.key.accAddress}};
-  let depositQueryResult = await queryContract(terra, ma_token_address, balanceQueryMsg);
+  const { balance: depositContractStartingBalance } = await queryContract(terra, ma_token_address, balanceQueryMsg);
 
-  if (depositQueryResult.balance !== "1000") {
-    throw new Error(`[Deposit]: expected to have balance = 1000 for address ${lpContractAddress}, got ${depositQueryResult.balance}`);
+  const depositMsg = {"deposit_native": {"symbol": "luna"}};
+  const depositAmount = 10000;
+  const coins = new Coin("uluna", depositAmount);
+  const executeDepositMsg = new MsgExecuteContract(wallet.key.accAddress, lpContractAddress, depositMsg, [coins]);
+  const depositTxResult = await performTransaction(terra, wallet, executeDepositMsg);
+
+  const { balance: depositContractEndingBalance } = await queryContract(terra, ma_token_address, balanceQueryMsg);
+  const depositContractDiff = depositContractEndingBalance - depositContractStartingBalance;
+
+  if (depositContractDiff !== depositAmount) {
+    throw new Error(`[Deposit]: expected to have luna balance increase by ${depositAmount} for address \
+    ${lpContractAddress}, got ${depositContractDiff}`);
   }
 
-  console.log("Query Result: ");
-  console.log(depositQueryResult);
+  let txInfo = await terra.tx.txInfo(depositTxResult.txhash);
+  const depositTxFee = Number(txInfo.tx.fee.amount._coins.uluna.amount);
+
+  let {_coins: {uluna: {amount: depositorEndingBalance}}} = await terra.bank.balance(wallet.key.accAddress);
+  let depositorBalanceDiff = depositorStartingBalance - depositorEndingBalance;
+
+  if (depositorBalanceDiff !== (depositAmount + depositTxFee)) {
+    throw new Error(`[Deposit]: expected depositor's balance to decrease by ${depositAmount + depositTxFee}, \
+    got ${depositorBalanceDiff}`);
+  }
+
+  console.log("Deposit Message Sent: ");
+  console.log(executeDepositMsg);
 
 
   console.log("### Testing Redeem...");
+  let {_coins: {uluna: {amount: redeemerStartingLunaBalance}}} = await terra.bank.balance(wallet.key.accAddress);
+
+  const senderMaLunaBalanceQueryMsg = {"balance": {"address": wallet.key.accAddress}};
+  let { balance: redeemerStartingMaLunaBalance} = await queryContract(terra, ma_token_address, senderMaLunaBalanceQueryMsg);
+
+  const redeemAmount = 5000;
   const executeMsg = {
     "send": {
       "contract": lpContractAddress,
-      "amount": "500",
+      "amount": redeemAmount.toString(),
       "msg": toEncodedBinary({ "redeem": {"id": "luna"} }),
     }
   };
 
   const sendMsg = new MsgExecuteContract(wallet.key.accAddress, ma_token_address, executeMsg);
   let redeemTxResult = await performTransaction(terra, wallet, sendMsg);
-  const lunaRedeemed = redeemTxResult.logs[0].events[3].attributes[2].value;
+  let redeemTxInfo = await terra.tx.txInfo(redeemTxResult.txhash);
+  const redeemTxFee = Number(redeemTxInfo.tx.fee.amount._coins.uluna.amount);
 
-  const senderBalanceQueryMsg = {"balance": {"address": wallet.key.accAddress}};
-  let senderBalanceResult = await queryContract(terra, ma_token_address, senderBalanceQueryMsg);
-  const maLunaBalance = senderBalanceResult.balance;
+  let { balance: redeemerEndingMaLunaBalance} = await queryContract(terra, ma_token_address, senderMaLunaBalanceQueryMsg);
+  const maLunaBalanceDiff = redeemerStartingMaLunaBalance - redeemerEndingMaLunaBalance;
 
-  if (lunaRedeemed !== "500uluna" || maLunaBalance !== "500") {
-    throw new Error(`[Redeem]: expected to have received 500 uluna and a remaining balance of 500 maluna. Received \
-  ${lunaRedeemed} uluna and have a remaining balance of ${maLunaBalance} maluna.`);
+  if (maLunaBalanceDiff !== redeemAmount) {
+    throw new Error(`[Redeem]: expected maluna balance to decrease by ${redeemAmount}, got ${maLunaBalanceDiff}`);
   }
 
-  console.log(`uluna redeemed: ${lunaRedeemed}`);
-  console.log(`remaining maluna balance: ${maLunaBalance}`);
+  let {_coins: {uluna: {amount: redeemerEndingLunaBalance}}} = await terra.bank.balance(wallet.key.accAddress);
+  const redeemerLunaBalanceDiff = redeemerEndingLunaBalance - redeemerStartingLunaBalance;
+
+  if (redeemerLunaBalanceDiff !== (redeemAmount - redeemTxFee)) {
+    throw new Error(`[Redeem]: expected depositor's balance to increase by ${redeemAmount - redeemTxFee}, \
+    got ${redeemerLunaBalanceDiff}`);
+  }
+
+  console.log("Redeem Message Sent:")
+  console.log(sendMsg);
 }
 
 main().catch(err => console.log(err));
