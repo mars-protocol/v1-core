dc696a4d145560cddebf21de84eb8699c4c17415
Ayush Menon <ayush@valon.com>
2021-03-27 19:54:38 -0400

    modified lib.rs to enable testing for mock_querier

--- contracts/liquidity-pool/src/lib.rs
+++ contracts/liquidity-pool/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod contract;
-mod mock_querier;
 pub mod msg;
 pub mod state;
+
+#[cfg(test)]
+mod mock_querier;
 
 #[cfg(all(target_arch = "wasm32", not(feature = "library")))]
 cosmwasm_std::create_entry_points_with_migration!(contract);
