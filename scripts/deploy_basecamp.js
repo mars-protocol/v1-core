6c93c698a449331552e6ae7fe1349c4f73ad3377
martianonthemoon <martianonthemoon123@protonmail.com>
2021-04-20 18:53:39 -0400

    mars and xmars basic tests

--- scripts/deploy_basecamp.js
+++ scripts/deploy_basecamp.js
@@ -1,46 +1,36 @@
 import 'dotenv/config.js';
-import {deployContract, recover, uploadContract} from "./helpers.mjs";
+import {deployBasecampContract, recover} from "./helpers.mjs";
 import {LCDClient, LocalTerra} from "@terra-money/terra.js";
 
-async function deployBasecamp(cooldownDuration, unstakeWindow, codeId=undefined) {
-  if (!codeId) {
-    console.log("Uploading Cw20 Contract...");
-    codeId = await uploadContract(terra, wallet, './artifacts/cw20_token.wasm');
+async function main() {
+  let terra;
+  let wallet;
+
+  if (process.env.NETWORK === "testnet") {
+    terra = new LCDClient({
+      URL: 'https://tequila-lcd.terra.dev',
+      chainID: 'tequila-0004'
+    })
+
+    wallet = await recover(terra, process.env.TEST_MAIN);
+    console.log(wallet.key.accAddress);
+  } else {
+    terra = new LocalTerra();
+    wallet = terra.wallets.test1;
   }
 
-  console.log("Deploying Basecamp...");
-  let initMsg = {"cw20_code_id": codeId, "cooldown_duration": cooldownDuration.toString(), "unstake_window": unstakeWindow.toString()};
-  let basecampContractAddress = await deployContract(terra, wallet, './artifacts/basecamp.wasm', initMsg);
+  let cooldownDuration;
+  let unstakeWindow;
 
-  console.log("Basecamp Contract Address: " + basecampContractAddress);
+  if (process.env.NETWORK === "testnet") {
+    cooldownDuration = 300;
+    unstakeWindow = 300;
+  } else {
+    cooldownDuration = 1;
+    unstakeWindow = 30;
+  }
+
+  await deployBasecampContract(terra, wallet, cooldownDuration, unstakeWindow);
 }
 
-let terra;
-let wallet;
-
-if (process.env.NETWORK === "testnet") {
-  terra = new LCDClient({
-    URL: 'https://tequila-lcd.terra.dev',
-    chainID: 'tequila-0004'
-  })
-
-  wallet = await recover(terra, process.env.TEST_MAIN);
-  console.log(wallet.key.accAddress);
-} else {
-  terra = new LocalTerra();
-  wallet = terra.wallets.test1;
-}
-
-let cooldownDuration;
-let unstakeWindow;
-
-if (process.env.NETWORK === "testnet") {
-  cooldownDuration = 300;
-  unstakeWindow = 300;
-} else {
-  cooldownDuration = 1;
-  unstakeWindow = 30;
-}
-
-await deployBasecamp(cooldownDuration, unstakeWindow);
-
+main().catch(console.log);
