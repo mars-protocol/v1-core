6c12aafe4aac3d42bfce4d05aebb02fdaab03110
MonteMonstera <monte.monstera@gmail.com>
2021-05-03 10:39:36 +0800

    Add ability to setup cw20 liquidity pool and update the testnet deploy scripts to match local deploy scripts

--- scripts/deploy_testnet.js
+++ scripts/deploy_testnet.js
@@ -1,17 +1,21 @@
 import 'dotenv/config.js';
+import { deployLiquidityPool, setupLiquidityPool, recover } from "./helpers.mjs";
 import { LCDClient } from "@terra-money/terra.js";
-import { deploy, setup, recover } from "./helpers.mjs";
 
 const terra = new LCDClient({
     URL: 'https://tequila-lcd.terra.dev',
     chainID: 'tequila-0004'
 })
 const wallet = await recover(terra, process.env.TEST_MAIN);
-let lpContractAddress = await deploy(terra, wallet);
 
+let returned_vals = await deployLiquidityPool(terra, wallet);
+
+// https://github.com/terra-project/assets/blob/master/cw20/tokens.json
 const initialAssets = [
     { denom: "uluna", borrow_slope: "0.1", loan_to_value: "0.5" },
-    { denom: "uusd", borrow_slope: "0.5", loan_to_value: "0.8" }
+    { denom: "uusd", borrow_slope: "0.5", loan_to_value: "0.8" },
+    { symbol: "ANC", contract_addr: "terra1747mad58h0w4y589y3sk84r5efqdev9q4r02pc", borrow_slope: "0.1", loan_to_value: "0.5" },
+    { symbol: "MIR", contract_addr: "terra10llyp6v3j3her8u3ce66ragytu45kcmd9asj3u", borrow_slope: "0.1", loan_to_value: "0.5" },
 ];
 
-await setup(terra, wallet, lpContractAddress, { initialAssets });
+await setupLiquidityPool(terra, wallet, returned_vals.lpAddress, { initialAssets });
