ecafa349ec6fdddfa7cf77c8efc94deb8140d836
martianonthemoon <martianonthemoon123@protonmail.com>
2021-05-19 14:52:16 -0400

    rename to treasury

--- contracts/treasury/examples/schema.rs
+++ contracts/treasury/examples/schema.rs
@@ -2,9 +2,9 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use reserve::msg::{ConfigResponse, HandleMsg, InitMsg, MigrateMsg, QueryMsg};
+use treasury::msg::{ConfigResponse, HandleMsg, InitMsg, MigrateMsg, QueryMsg};
 
-use reserve::state::Config;
+use treasury::state::Config;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
