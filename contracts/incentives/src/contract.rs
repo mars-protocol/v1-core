8cace4f6ec9ba8d2b0ca8d447e2e058a272012fc
Spike Spiegel <spikeonmars@protonmail.com>
2021-06-10 16:06:53 -0300

    Layout set asset incentives

--- contracts/incentives/src/contract.rs
+++ contracts/incentives/src/contract.rs
@@ -1,10 +1,12 @@
 use cosmwasm_std::{
-    log, to_binary, Api, Binary, CosmosMsg, Env, Extern, HandleResponse, InitResponse,
-    MigrateResponse, MigrateResult, Querier, StdError, StdResult, Storage,
+    log, to_binary, Api, Binary, CosmosMsg, Decimal, Env, Extern, HandleResponse, HumanAddr,
+    InitResponse, MigrateResponse, MigrateResult, Querier, StdError, StdResult, Storage, Uint128,
 };
 
-use crate::msg::{ConfigResponse, HandleMsg, InitMsg, MigrateMsg, QueryMsg};
-use crate::state::{config_state, config_state_read, Config};
+use crate::msg::{ConfigResponse, HandleMsg, InitMsg, MigrateMsg, QueryMsg, SetAssetIncentive};
+
+use crate::state;
+use crate::state::{AssetIncentive, Config};
 
 // INIT
 
@@ -18,7 +20,7 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
         owner: deps.api.canonical_address(&env.message.sender)?,
     };
 
-    config_state(&mut deps.storage).save(&config)?;
+    state::config(&mut deps.storage).save(&config)?;
 
     Ok(InitResponse {
         log: vec![],
@@ -34,8 +36,62 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
     match msg {
+        HandleMsg::SetAssetIncentives {
+            set_asset_incentives,
+        } => handle_set_asset_incentives(deps, env, set_asset_incentives),
+        HandleMsg::HandleBalanceChange {
+            user_address,
+            user_balance,
+            total_supply,
+        } => handle_balance_change(deps, env, user_address, user_balance, total_supply),
         HandleMsg::ExecuteCosmosMsg(cosmos_msg) => handle_execute_cosmos_msg(deps, env, cosmos_msg),
     }
+}
+
+/// Sets emission per second for asset incentives
+/// If there are existing incentives for the asset, index is updated
+pub fn handle_balance_change<S: Storage, A: Api, Q: Querier>(
+    _deps: &mut Extern<S, A, Q>,
+    _env: Env,
+    _user_address: HumanAddr,
+    _user_balance: Uint128,
+    _total_supply: Uint128,
+) -> StdResult<HandleResponse> {
+    Ok(HandleResponse::default())
+}
+
+/// Sets emission per second for asset incentives
+/// If there are existing incentives for the asset, index is updated
+pub fn handle_set_asset_incentives<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    set_asset_incentives: Vec<SetAssetIncentive>,
+) -> StdResult<HandleResponse> {
+    let mut asset_incentives_bucket = state::asset_incentives(&mut deps.storage);
+    for set_asset_incentive in set_asset_incentives {
+        let ma_asset_canonical_address = deps
+            .api
+            .canonical_address(&set_asset_incentive.ma_token_address)?;
+        let new_asset_incentive =
+            match asset_incentives_bucket.may_load(&ma_asset_canonical_address.as_slice())? {
+                Some(mut asset_incentive) => {
+                    // TODO: Update index
+                    asset_incentive.emission_per_second = set_asset_incentive.emission_per_second;
+                    asset_incentive.last_updated_timestamp = env.block.time;
+                    asset_incentive
+                }
+                None => AssetIncentive {
+                    emission_per_second: set_asset_incentive.emission_per_second,
+                    index: Decimal::one(),
+                    last_updated_timestamp: env.block.time,
+                },
+            };
+        asset_incentives_bucket
+            .save(&ma_asset_canonical_address.as_slice(), &new_asset_incentive)?;
+    }
+
+    //TODO: Maybe log this?
+    Ok(HandleResponse::default())
 }
 
 /// Execute Cosmos message
@@ -44,7 +100,7 @@ pub fn handle_execute_cosmos_msg<S: Storage, A: Api, Q: Querier>(
     env: Env,
     msg: CosmosMsg,
 ) -> StdResult<HandleResponse> {
-    let config = config_state_read(&deps.storage).load()?;
+    let config = state::config_read(&deps.storage).load()?;
 
     if deps.api.canonical_address(&env.message.sender)? != config.owner {
         return Err(StdError::unauthorized());
@@ -71,7 +127,7 @@ pub fn query<S: Storage, A: Api, Q: Querier>(
 fn query_config<S: Storage, A: Api, Q: Querier>(
     deps: &Extern<S, A, Q>,
 ) -> StdResult<ConfigResponse> {
-    let config = config_state_read(&deps.storage).load()?;
+    let config = state::config_read(&deps.storage).load()?;
     Ok(ConfigResponse {
         owner: deps.api.human_address(&config.owner)?,
     })
@@ -95,8 +151,6 @@ mod tests {
     use cosmwasm_std::{BankMsg, Coin, CosmosMsg, HumanAddr, Uint128};
     use mars::testing::{mock_dependencies, mock_env, MockEnvParams};
 
-    use crate::state::config_state_read;
-
     #[test]
     fn test_proper_initialization() {
         let mut deps = mock_dependencies(20, &[]);
@@ -108,7 +162,7 @@ mod tests {
         let empty_vec: Vec<CosmosMsg> = vec![];
         assert_eq!(empty_vec, res.messages);
 
-        let config = config_state_read(&deps.storage).load().unwrap();
+        let config = state::config_read(&deps.storage).load().unwrap();
         assert_eq!(
             deps.api
                 .canonical_address(&HumanAddr::from("owner"))
