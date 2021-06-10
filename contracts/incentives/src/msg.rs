f5630acd313c304f5251522c87b972d9aa391e53
Spike Spiegel <spikeonmars@protonmail.com>
2021-06-10 16:06:53 -0300

    Layout set asset incentives

--- contracts/incentives/src/msg.rs
+++ contracts/incentives/src/msg.rs
@@ -1,15 +1,37 @@
-use cosmwasm_std::{CosmosMsg, HumanAddr};
+use cosmwasm_std::{CosmosMsg, HumanAddr, Uint128};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {}
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
+    /// Set emission per second for a list of assets
+    SetAssetIncentives {
+        set_asset_incentives: Vec<SetAssetIncentive>,
+    },
+
+    /// Handle a balance change. Sent on an external contract,
+    /// triggered on user balance changes
+    HandleBalanceChange {
+        user_address: HumanAddr,
+        user_balance: Uint128,
+        total_supply: Uint128,
+    },
+
     /// Execute Cosmos msg
     ExecuteCosmosMsg(CosmosMsg),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
+pub struct SetAssetIncentive {
+    /// Ma token address associated with the incentives
+    pub ma_token_address: HumanAddr,
+    /// How many Mars will be assigned per second to be distributed among all liquidity
+    /// providers
+    pub emission_per_second: Uint128,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
--- contracts/incentives/src/state.rs
+++ contracts/incentives/src/state.rs
@@ -1,11 +1,18 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CanonicalAddr, Storage};
-use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+use cosmwasm_std::{CanonicalAddr, Decimal, Storage, Uint128};
+use cosmwasm_storage::{
+    bucket, bucket_read, singleton, singleton_read, Bucket, ReadonlyBucket, ReadonlySingleton,
+    Singleton,
+};
 
 // keys (for singleton)
 pub static CONFIG_KEY: &[u8] = b"config";
+
+// namespaces (for buckets)
+pub static ASSET_DATA_NAMESPACE: &[u8] = b"asset_data";
+pub static ASSET_USER_INDEXES_NAMESPACE: &[u8] = b"asset_user_indexes";
 
 /// Insurance fund global configuration
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -13,10 +20,25 @@ pub struct Config {
     pub owner: CanonicalAddr,
 }
 
-pub fn config_state<S: Storage>(storage: &mut S) -> Singleton<S, Config> {
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct AssetIncentive {
+    pub emission_per_second: Uint128,
+    pub index: Decimal,
+    pub last_updated_timestamp: u64,
+}
+
+pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, Config> {
     singleton(storage, CONFIG_KEY)
 }
 
-pub fn config_state_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, Config> {
+pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, Config> {
     singleton_read(storage, CONFIG_KEY)
+}
+
+pub fn asset_incentives<S: Storage>(storage: &mut S) -> Bucket<S, AssetIncentive> {
+    bucket(ASSET_DATA_NAMESPACE, storage)
+}
+
+pub fn asset_incentives_read<S: Storage>(storage: &S) -> ReadonlyBucket<S, AssetIncentive> {
+    bucket_read(ASSET_DATA_NAMESPACE, storage)
 }
