0db54ba9fea6a3d55b52e00ed5a34df0fc73a24e
Spike Spiegel <spikeonmars@protonmail.com>
2021-06-15 17:02:50 -0300

    Move staking msg to a package

--- contracts/staking/examples/schema.rs
+++ contracts/staking/examples/schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use staking::msg::{ConfigResponse, HandleMsg, InitMsg, MigrateMsg, QueryMsg};
+use mars::staking::msg::{ConfigResponse, HandleMsg, InitMsg, MigrateMsg, QueryMsg};
 
 use staking::state::Config;
 
--- packages/mars/src/lib.rs
+++ packages/mars/src/lib.rs
@@ -2,6 +2,7 @@ pub mod cw20_token;
 pub mod helpers;
 pub mod liquidity_pool;
 pub mod ma_token;
+pub mod staking;
 pub mod storage;
 pub mod swapping;
 pub mod xmars_token;
