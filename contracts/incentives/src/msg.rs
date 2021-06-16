3909cf9f2d01f07563738f6ffe3b5a81246e671b
Spike Spiegel <spikeonmars@protonmail.com>
2021-06-16 13:13:42 -0300

    Set only a single asset instead of multiple, add access control and logging to handle_set_asset_incentive

--- contracts/incentives/src/msg.rs
+++ contracts/incentives/src/msg.rs
@@ -12,20 +12,23 @@ pub struct InitMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    /// Set emission per second for a list of assets
-    /// set_asset_incentives: list of asset incentives to initilize/update
-    SetAssetIncentives {
-        set_asset_incentives: Vec<SetAssetIncentive>,
+    /// Set emission per second for an asset
+    /// ma_token_address
+    SetAssetIncentive {
+        /// Ma token address associated with the incentives
+        ma_token_address: HumanAddr,
+        /// How many Mars will be assigned per second to be distributed among all liquidity
+        /// providers
+        emission_per_second: Uint128,
     },
 
     /// Handle a balance change. Sent on an external contract,
     /// triggered on user balance changes
-    /// user_address: address of the user
-    /// user_balance: user balance up to the instant before the change
-    /// total_supply: total supply up to the instant before the change
     HandleBalanceChange {
         user_address: HumanAddr,
+        /// user balance up to the instant before the change
         user_balance_before: Uint128,
+        /// total supply up to the instant before the change
         total_supply_before: Uint128,
     },
 
@@ -42,15 +45,6 @@ pub enum HandleMsg {
 
     /// Execute Cosmos msg. Only callable by owner
     ExecuteCosmosMsg(CosmosMsg),
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
-pub struct SetAssetIncentive {
-    /// Ma token address associated with the incentives
-    pub ma_token_address: HumanAddr,
-    /// How many Mars will be assigned per second to be distributed among all liquidity
-    /// providers
-    pub emission_per_second: Uint128,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
