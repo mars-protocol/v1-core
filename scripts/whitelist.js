5c017065236cf158762d89904fb8643216b7adaa
MonteMonstera <monte.monstera@gmail.com>
2021-07-16 11:58:22 +0800

    Update whitelist.json to work with red bank contract refactor, and create a whitelist dir in artifacts if doesn't already exist

--- scripts/whitelist.js
+++ scripts/whitelist.js
@@ -1,11 +1,11 @@
 import 'dotenv/config.js';
-import {queryContract} from "./helpers.mjs";
-import {LCDClient, LocalTerra} from "@terra-money/terra.js";
-import {writeFileSync} from 'fs';
+import { queryContract } from "./helpers.mjs";
+import { LCDClient, LocalTerra } from "@terra-money/terra.js";
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
 
 async function main() {
   let terra;
-  let lpContractAddress = process.env.LP_ADDRESS;
+  let lpContractAddress = process.env.REDBANK_ADDRESS;
 
   if (process.env.NETWORK === "testnet") {
     terra = new LCDClient({
@@ -16,23 +16,29 @@ async function main() {
     terra = new LocalTerra();
   }
 
-  const reservesListResult = await queryContract(terra, lpContractAddress, {"reserves_list": {}});
-  const { reserves_list } = reservesListResult;
-  const reserveInfo = {};
+  const marketsListResult = await queryContract(terra, lpContractAddress, { "markets_list": {} });
+  const { markets_list } = marketsListResult;
+  const marketInfo = {};
 
-  for (let reserve of reserves_list) {
-    const {denom, ma_token_address} = reserve;
-    const tokenInfoQuery = {"token_info": {}};
+  for (let market of markets_list) {
+    const { denom, ma_token_address } = market;
+    const tokenInfoQuery = { "token_info": {} };
     let { decimals } = await queryContract(terra, ma_token_address, tokenInfoQuery);
-    reserveInfo[ma_token_address] = {denom, decimals}
+    marketInfo[ma_token_address] = { denom, decimals }
   }
 
   const output = {};
-  output.contracts = {lpContractAddress};
-  output.whitelist = reserveInfo;
+  output.contracts = { lpContractAddress };
+  output.whitelist = marketInfo;
 
   const json = JSON.stringify(output);
-  writeFileSync(`artifacts/whitelists/${process.env.NETWORK || 'localterra'}.json`, json, {'encoding': 'utf8'});
+
+  const dir = "artifacts/whitelists"
+  const fileName = `${process.env.NETWORK || 'localterra'}.json`
+  if (!existsSync(dir)) {
+    mkdirSync(dir);
+  }
+  writeFileSync(`${dir}/${fileName}`, json, { 'encoding': 'utf8' });
 }
 
 main().catch(err => console.log(err));
