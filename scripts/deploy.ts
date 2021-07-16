5cc557f545f2e4fd85234cfde8a0290c648409c9
harryscholes <harryscholes@users.noreply.github.com>
2021-07-16 15:11:28 +0100

    Formatting

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -9,7 +9,7 @@ import {
   setupRedBank,
   uploadContract,
 } from "./helpers.js";
-import { LCDClient, LocalTerra } from "@terra-money/terra.js";
+import { LCDClient, LocalTerra, Wallet } from "@terra-money/terra.js";
 import { Config, testnet, local } from "./deploy_configs.js"
 import { join } from "path"
 
@@ -21,7 +21,7 @@ const MARS_ARTIFACTS_PATH = "../artifacts"
 
 async function main() {
   let terra;
-  let wallet;
+  let wallet: Wallet;
   const isTestnet = process.env.NETWORK === "testnet"
 
   if (isTestnet) {
@@ -47,13 +47,23 @@ async function main() {
 
   /*************************************** Deploy Address Provider Contract *****************************************/
   console.log("Deploying Address Provider...");
-  const addressProviderContractAddress = await deployContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'address_provider.wasm'), { "owner": wallet.key.accAddress })
+  const addressProviderContractAddress = await deployContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'address_provider.wasm'),
+    { "owner": wallet.key.accAddress },
+  )
   console.log("Address Provider Contract Address: " + addressProviderContractAddress);
 
   /*************************************** Deploy Council Contract *****************************************/
   console.log("Deploying council...");
   deployConfig.councilInitMsg.config.address_provider_address = addressProviderContractAddress
-  const councilContractAddress = await deployContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'council.wasm'), deployConfig.councilInitMsg);
+  const councilContractAddress = await deployContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'council.wasm'),
+    deployConfig.councilInitMsg,
+  );
   console.log("Council Contract Address: " + councilContractAddress);
 
   /**************************************** Deploy Staking Contract *****************************************/
@@ -61,60 +71,120 @@ async function main() {
   // TODO fix `factory_contract_address` in LocalTerra
   deployConfig.stakingInitMsg.config.owner = councilContractAddress
   deployConfig.stakingInitMsg.config.address_provider_address = addressProviderContractAddress
-  const stakingContractAddress = await deployContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'staking.wasm'), deployConfig.stakingInitMsg);
+  const stakingContractAddress = await deployContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'staking.wasm'),
+    deployConfig.stakingInitMsg,
+  );
   console.log("Staking Contract Address: " + stakingContractAddress);
 
   /************************************* Deploy Insurance Fund Contract *************************************/
   console.log("Deploying Insurance Fund...");
   deployConfig.insuranceFundInitMsg.owner = councilContractAddress
-  const insuranceFundContractAddress = await deployContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'insurance_fund.wasm'), deployConfig.insuranceFundInitMsg)
+  const insuranceFundContractAddress = await deployContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'insurance_fund.wasm'),
+    deployConfig.insuranceFundInitMsg,
+  )
   console.log("Insurance Fund Contract Address: " + insuranceFundContractAddress);
 
   /**************************************** Deploy Treasury Contract ****************************************/
   console.log("Deploying Treasury...");
-  const treasuryContractAddress = await deployContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'treasury.wasm'), { "owner": councilContractAddress })
+  const treasuryContractAddress = await deployContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'treasury.wasm'),
+    { "owner": councilContractAddress },
+  )
   console.log("Treasury Contract Address: " + treasuryContractAddress);
 
   /**************************************** Deploy Incentives Contract ****************************************/
   console.log("Deploying Incentives...");
-  const incentivesContractAddress = await deployContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'incentives.wasm'), { "owner": councilContractAddress, "address_provider_address": addressProviderContractAddress })
+  const incentivesContractAddress = await deployContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'incentives.wasm'),
+    {
+      "owner": councilContractAddress,
+      "address_provider_address": addressProviderContractAddress
+    },
+  )
   console.log("Incentives Contract Address: " + incentivesContractAddress);
 
   /************************************* Upload cw20 Token Contract *************************************/
   console.log("Uploading cw20 token contract");
-  const cw20TokenCodeId = await uploadContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'cw20_token.wasm'));
+  const cw20TokenCodeId = await uploadContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'cw20_token.wasm'),
+  );
   console.log(`Uploaded cw20 token contract, code: ${cw20TokenCodeId}`);
 
   /************************************* Instantiate Mars Token Contract *************************************/
   console.log("Deploying Mars token...");
-  const marsTokenContractAddress = await instantiateContract(terra, wallet, cw20TokenCodeId, {
-    "name": "Mars token",
-    symbol: "Mars",
-    decimals: 6,
-    initial_balances: isTestnet ? [{ "address": wallet.key.accAddress, "amount": "1000000000000" }] : [],
-    mint: { "minter": councilContractAddress },
-  });
+  const marsTokenContractAddress = await instantiateContract(
+    terra,
+    wallet,
+    cw20TokenCodeId,
+    {
+      "name": "Mars token",
+      symbol: "Mars",
+      decimals: 6,
+      initial_balances: isTestnet ? [
+        {
+          "address": wallet.key.accAddress,
+          "amount": "1000000000000"
+        }
+      ] : [],
+      mint: {
+        "minter": councilContractAddress
+      },
+    }
+  );
   console.log("Mars Token Contract Address: " + marsTokenContractAddress);
 
-  const balanceResponse = await queryContract(terra, marsTokenContractAddress, { "balance": { "address": wallet.key.accAddress } })
+  const balanceResponse = await queryContract(
+    terra,
+    marsTokenContractAddress,
+    {
+      "balance": {
+        "address": wallet.key.accAddress
+      }
+    }
+  )
   console.log(`Balance of adress ${wallet.key.accAddress}: ${balanceResponse.balance / 1e6} Mars`)
 
   /************************************* Instantiate xMars Token Contract *************************************/
   console.log("Deploying xMars token...");
-  const xMarsTokenCodeId = await uploadContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'xmars_token.wasm'));
+  const xMarsTokenCodeId = await uploadContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'xmars_token.wasm')
+  );
   console.log(`Uploaded xMars token contract, code: ${xMarsTokenCodeId}`);
-  const xMarsTokenContractAddress = await instantiateContract(terra, wallet, xMarsTokenCodeId, {
-    "name": "xMars token",
-    symbol: "xMars",
-    decimals: 6,
-    initial_balances: [],
-    mint: { "minter": stakingContractAddress },
-  });
+  const xMarsTokenContractAddress = await instantiateContract(
+    terra,
+    wallet,
+    xMarsTokenCodeId,
+    {
+      "name": "xMars token",
+      symbol: "xMars",
+      decimals: 6,
+      initial_balances: [],
+      mint: { "minter": stakingContractAddress },
+    }
+  );
   console.log("xMars Token Contract Address: " + xMarsTokenContractAddress);
 
   /************************************* Upload ma_token Token Contract *************************************/
   console.log("Uploading ma_token contract");
-  const maTokenCodeId = await uploadContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'ma_token.wasm'))
+  const maTokenCodeId = await uploadContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'ma_token.wasm')
+  )
   console.log(`Uploaded ma_token contract code: ${maTokenCodeId}`);
 
   /************************************* Deploy Red Bank Contract *************************************/
@@ -122,36 +192,60 @@ async function main() {
   deployConfig.redBankInitMsg.config.owner = wallet.key.accAddress
   deployConfig.redBankInitMsg.config.address_provider_address = addressProviderContractAddress
   deployConfig.redBankInitMsg.config.ma_token_code_id = maTokenCodeId
-  const redBankContractAddress = await deployContract(terra, wallet, join(MARS_ARTIFACTS_PATH, 'red_bank.wasm'), deployConfig.redBankInitMsg)
+  const redBankContractAddress = await deployContract(
+    terra,
+    wallet,
+    join(MARS_ARTIFACTS_PATH, 'red_bank.wasm'),
+    deployConfig.redBankInitMsg,
+  )
   console.log(`Red Bank Contract Address: ${redBankContractAddress}`);
 
   /**************************************** Update Config in Address Provider Contract *****************************************/
   console.log('Setting addresses in address provider')
-  await executeContract(terra, wallet, addressProviderContractAddress, {
-    "update_config": {
-      "config": {
-        "owner": councilContractAddress,
-        "council_address": councilContractAddress,
-        "incentives_address": incentivesContractAddress,
-        "insurance_fund_address": insuranceFundContractAddress,
-        "mars_token_address": marsTokenContractAddress,
-        "red_bank_address": redBankContractAddress,
-        "staking_address": stakingContractAddress,
-        "treasury_address": treasuryContractAddress,
-        "xmars_token_address": xMarsTokenContractAddress
+  await executeContract(
+    terra,
+    wallet,
+    addressProviderContractAddress,
+    {
+      "update_config": {
+        "config": {
+          "owner": councilContractAddress,
+          "council_address": councilContractAddress,
+          "incentives_address": incentivesContractAddress,
+          "insurance_fund_address": insuranceFundContractAddress,
+          "mars_token_address": marsTokenContractAddress,
+          "red_bank_address": redBankContractAddress,
+          "staking_address": stakingContractAddress,
+          "treasury_address": treasuryContractAddress,
+          "xmars_token_address": xMarsTokenContractAddress
+        }
       }
     }
-  })
+  )
   console.log("Address Provider config successfully setup: ", await queryContract(terra, addressProviderContractAddress, { "config": {} }))
 
   /************************************* Setup Initial Liquidity Pools **************************************/
-  await setupRedBank(terra, wallet, redBankContractAddress, { initialAssets: deployConfig.initialAssets });
+  await setupRedBank(
+    terra,
+    wallet,
+    redBankContractAddress,
+    {
+      initialAssets: deployConfig.initialAssets
+    }
+  );
   console.log("Initial assets setup successfully")
 
   // Once initial assets initialized, set the owner of Red Bank to be Council rather than EOA
   console.log(`Updating Red Bank to be owned by Council contract ${councilContractAddress}`)
   deployConfig.redBankInitMsg.config.owner = councilContractAddress
-  await executeContract(terra, wallet, redBankContractAddress, { "update_config": deployConfig.redBankInitMsg })
+  await executeContract(
+    terra,
+    wallet,
+    redBankContractAddress,
+    {
+      "update_config": deployConfig.redBankInitMsg
+    }
+  )
   console.log("Red Bank config successfully updated: ", await queryContract(terra, redBankContractAddress, { "config": {} }))
 }
 
