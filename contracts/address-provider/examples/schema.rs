46c87309bba705e65e70631e2ce4fa1210ad8650
Spike Spiegel <spikeonmars@protonmail.com>
2021-08-15 08:58:47 -0300

    Remove MigrateMsg from contracts's schema.rs

--- contracts/address-provider/examples/schema.rs
+++ contracts/address-provider/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 use mars::address_provider::msg::{
-    ConfigResponse, ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg,
+    ConfigResponse, ExecuteMsg, InstantiateMsg, QueryMsg,
 };
 
 use address_provider::state::Config;
@@ -19,5 +19,4 @@ fn main() {
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(Config), &out_dir);
     export_schema(&schema_for!(ConfigResponse), &out_dir);
-    export_schema(&schema_for!(MigrateMsg), &out_dir);
 }
