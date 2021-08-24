3dee80ca59b3e43b4876dfefdafbfcef300cc706
harryscholes <harryscholes@users.noreply.github.com>
2021-08-24 09:44:20 +0100

    Integration tests: upgrade insurance fund tests to col-5

--- scripts/tests/insurance_fund.ts
+++ scripts/tests/insurance_fund.ts
@@ -9,16 +9,7 @@ $ tree -L 1 $(git rev-parse --show-toplevel)/..
 ├── protocol
 ├── terraswap
 ```
-
-This test works on columbus-4 with the following versions:
-- LocalTerra 1c3f42a60116b4c17cb5d002aa194eae9b8811b5
-- terracli v0.5.0-rc0
-- terraswap 72c60c05c43841499f760710a03f864c5ee4db3b
-
-TODO:
-- Upgrade to columbus-5
 */
-
 import { Int, LocalTerra, MsgSend, Numeric, Wallet } from "@terra-money/terra.js"
 import {
   deployContract,
@@ -27,13 +18,22 @@ import {
   mustPerformTransaction,
   queryContract,
   setTimeoutDuration,
-  toEncodedBinary,
   uploadContract
 } from "../helpers.js"
 import { strict as assert, strictEqual } from "assert"
 import { join } from "path"
 
-// types
+// CONSTS
+
+const ZERO = new Int(0)
+const MARS_ARTIFACTS_PATH = "../artifacts"
+const TERRASWAP_ARTIFACTS_PATH = "../../terraswap/artifacts"
+const TOKEN_SUPPLY = 1_000_000_000_000000
+const TOKEN_LP = 10_000_000_000000
+const USD_LP = 1_000_000_000000
+const INSURANCE_FUND_TOKEN_BALANCE = 100_000_000000
+
+// TYPES
 
 interface NativeToken {
   native_token: {
@@ -59,41 +59,20 @@ interface Env {
   insuranceFundAddress: string,
 }
 
-// consts and globals
+// HELPERS
 
-const ZERO = new Int(0)
-const MARS_ARTIFACTS_PATH = "../artifacts"
-const TERRASWAP_ARTIFACTS_PATH = "../../terraswap/artifacts"
-const TOKEN_SUPPLY = 1_000_000_000_000000
-const TOKEN_LP = 10_000_000_000000
-const USD_LP = 1_000_000_000000
-const INSURANCE_FUND_TOKEN_BALANCE = 100_000_000000
-
-// helpers
-
-async function instantiateUsdPair(env: Env, bid: Token): Promise<string> {
-  return await instantiateContract(env.terra, env.wallet, env.factoryCodeID,
+async function instantiateUsdPair(env: Env, bid: Token) {
+  const result = await executeContract(env.terra, env.wallet, env.factoryAddress,
     {
-      "pair_code_id": env.pairCodeID,
-      "token_code_id": env.tokenCodeID,
-      "init_hook": {
-        "msg": toEncodedBinary(
-          {
-            "create_pair": {
-              "asset_infos": [
-                bid, {
-                  "native_token": {
-                    "denom": "uusd"
-                  }
-                }
-              ]
-            }
-          }
-        ),
-        "contract_addr": env.factoryAddress
+      create_pair: {
+        asset_infos: [
+          bid,
+          { "native_token": { "denom": "uusd" } }
+        ]
       }
     }
   )
+  return result.logs[0].eventsByType.wasm.pair_contract_addr[0]
 }
 
 async function provideLiquidity(env: Env, address: string, token: Token, coins: string) {
@@ -105,11 +84,7 @@ async function provideLiquidity(env: Env, address: string, token: Token, coins: 
             "info": token,
             "amount": String(TOKEN_LP)
           }, {
-            "info": {
-              "native_token": {
-                "denom": "uusd"
-              }
-            },
+            "info": { "native_token": { "denom": "uusd" } },
             "amount": String(USD_LP)
           }
         ]
@@ -128,17 +103,14 @@ async function getBalance(env: Env, address: string, denom: string): Promise<Num
   return balance.amount
 }
 
-// tests
+// TESTS
 
 async function testSwapNativeTokenToUsd(env: Env, denom: string) {
-  const NATIVE_TOKEN = {
-    "native_token": {
-      "denom": denom
-    }
-  }
+  const NATIVE_TOKEN = { "native_token": { "denom": denom } }
 
   // instantiate a native token/USD Terraswap pair
   const pairAddress = await instantiateUsdPair(env, NATIVE_TOKEN)
+
   await provideLiquidity(env, pairAddress, NATIVE_TOKEN, `${USD_LP}uusd,${TOKEN_LP}${denom}`)
 
   // transfer some native token to the insurance fund
@@ -172,21 +144,13 @@ async function testSwapNativeTokenToUsd(env: Env, denom: string) {
   strictEqual(tokenBalance, ZERO)
 
   // check the Terraswap pair balances
-  const pool = await queryContract(env.terra, pairAddress,
-    {
-      "pool": {}
-    }
-  )
+  const pool = await queryContract(env.terra, pairAddress, { "pool": {} })
   strictEqual(pool.assets[0].amount, String(TOKEN_LP + INSURANCE_FUND_TOKEN_BALANCE))
   assert(parseInt(pool.assets[1].amount) < USD_LP)
 }
 
 async function testSwapTokenToUsd(env: Env, address: string) {
-  const TOKEN = {
-    "token": {
-      "contract_addr": address
-    }
-  }
+  const TOKEN = { "token": { "contract_addr": address } }
 
   // instantiate a token/USD Terraswap pair
   const pairAddress = await instantiateUsdPair(env, TOKEN)
@@ -227,26 +191,16 @@ async function testSwapTokenToUsd(env: Env, address: string) {
   // check the insurance fund balances
   const usdBalance = await getBalance(env, env.insuranceFundAddress, "uusd")
   assert(usdBalance.gt(prevUsdBalance))
-  const tokenBalance = await queryContract(env.terra, address,
-    {
-      "balance": {
-        "address": env.insuranceFundAddress
-      }
-    }
-  )
+  const tokenBalance = await queryContract(env.terra, address, { "balance": { "address": env.insuranceFundAddress } })
   strictEqual(tokenBalance.balance, "0")
 
   // check the Terraswap pair balances
-  const pool = await queryContract(env.terra, pairAddress,
-    {
-      "pool": {}
-    }
-  )
+  const pool = await queryContract(env.terra, pairAddress, { "pool": {} })
   strictEqual(pool.assets[0].amount, String(TOKEN_LP + INSURANCE_FUND_TOKEN_BALANCE))
   assert(parseInt(pool.assets[1].amount) < USD_LP)
 }
 
-// main
+// MAIN
 
 async function main() {
   setTimeoutDuration(0)
