87c7e7102fe2f0522ead1efbd404966c3a777dd4
Harry Scholes <harryscholes@users.noreply.github.com>
2021-09-09 15:50:22 +0100

    council: add schema for ReceiveMsg and ProposalMessage

--- contracts/council/examples/schema.rs
+++ contracts/council/examples/schema.rs
@@ -2,9 +2,10 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use council::msg::{ConfigResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use council::msg::{ConfigResponse, ExecuteMsg, InstantiateMsg, QueryMsg, ReceiveMsg};
 
 use council::state::Config;
+use council::types::ProposalMessage;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -14,6 +15,8 @@ fn main() {
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(ProposalMessage), &out_dir);
+    export_schema(&schema_for!(ReceiveMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(Config), &out_dir);
     export_schema(&schema_for!(ConfigResponse), &out_dir);
