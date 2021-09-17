42889d69530b9bb6a8a64115aa5defbe3741f26b
harryscholes <harryscholes@users.noreply.github.com>
2021-09-17 16:50:03 +0100

    Integration tests: rewards distribution

---
+++ scripts/tests/rewards_distribution.ts
@@ -0,0 +1,579 @@
+import { Coin, Int, LCDClient, LocalTerra, Wallet } from "@terra-money/terra.js"
+import { strictEqual, strict as assert } from "assert"
+import { join } from "path"
+import 'dotenv/config.js'
+import { deployContract, executeContract, queryContract, setTimeoutDuration, toEncodedBinary, uploadContract } from "../helpers.js"
+
+// CONSTS
+
+// required environment variables:
+const CW_PLUS_ARTIFACTS_PATH = process.env.CW_PLUS_ARTIFACTS_PATH!
+const TERRASWAP_ARTIFACTS_PATH = process.env.TERRASWAP_ARTIFACTS_PATH!
+
+// protocol rewards collector
+const SAFETY_FUND_FEE_SHARE = 0.1
+const TREASURY_FEE_SHARE = 0.2
+
+// red-bank
+const CLOSE_FACTOR = 0.5
+const MAX_LTV = 0.55
+const LIQUIDATION_BONUS = 0.1
+const MA_TOKEN_SCALING_FACTOR = 1_000_000
+// set a high interest rate, so tests can be run faster
+const INTEREST_RATE = 100000
+
+// native tokens
+const LUNA_USD_PRICE = 25
+const USD_COLLATERAL_AMOUNT = 100_000_000_000000
+const LUNA_COLLATERAL_AMOUNT = 1_000_000000
+const USD_BORROW_AMOUNT = LUNA_COLLATERAL_AMOUNT * LUNA_USD_PRICE * MAX_LTV
+
+// cw20 tokens
+const CW20_TOKEN_USD_PRICE = 10
+const CW20_TOKEN_1_COLLATERAL_AMOUNT = 100_000_000_000000
+const CW20_TOKEN_2_COLLATERAL_AMOUNT = 1_000_000000
+const CW20_TOKEN_1_BORROW_AMOUNT = CW20_TOKEN_2_COLLATERAL_AMOUNT * MAX_LTV
+
+// HELPERS
+
+async function queryMaAssetAddress(terra: LCDClient, redBank: string, asset: Asset): Promise<string> {
+  const market = await queryContract(terra, redBank, { market: { asset: asset } })
+  return market.ma_token_address
+}
+
+async function setAssetOraclePriceSource(terra: LCDClient, wallet: Wallet, oracle: string, asset: Asset, price: number) {
+  await executeContract(terra, wallet, oracle,
+    {
+      set_asset: {
+        asset: asset,
+        price_source: { fixed: { price: String(price) } }
+      }
+    }
+  )
+}
+
+async function depositNative(terra: LCDClient, wallet: Wallet, redBank: string, denom: string, amount: number) {
+  return await executeContract(terra, wallet, redBank,
+    { deposit_native: { denom: denom } },
+    `${amount}${denom}`
+  )
+}
+
+async function depositCw20(terra: LCDClient, wallet: Wallet, redBank: string, contract: string, amount: number) {
+  return await executeContract(terra, wallet, contract,
+    {
+      send: {
+        contract: redBank,
+        amount: String(amount),
+        msg: toEncodedBinary({ deposit_cw20: {} })
+      }
+    }
+  )
+}
+
+async function borrowNative(terra: LCDClient, wallet: Wallet, redBank: string, denom: string, amount: number) {
+  return await executeContract(terra, wallet, redBank,
+    {
+      borrow: {
+        asset: { native: { denom: denom } },
+        amount: String(amount)
+      }
+    }
+  )
+}
+
+async function borrowCw20(terra: LCDClient, wallet: Wallet, redBank: string, contract: string, amount: number) {
+  return await executeContract(terra, wallet, redBank,
+    {
+      borrow: {
+        asset: { cw20: { contract_addr: contract } },
+        amount: String(amount)
+      }
+    }
+  )
+}
+
+async function queryNativeBalance(terra: LCDClient, address: string, denom: string) {
+  const balances = await terra.bank.balance(address)
+  const balance = balances.get(denom)
+  if (balance === undefined) {
+    return 0
+  }
+  return balance.amount.toNumber()
+}
+
+async function queryCw20Balance(terra: LCDClient, userAddress: string, contractAddress: string) {
+  const result = await queryContract(terra, contractAddress, { balance: { address: userAddress } })
+  return parseInt(result.balance)
+}
+
+async function computeTax(terra: LCDClient, coin: Coin) {
+  const DECIMAL_FRACTION = new Int("1000000000000000000") // 10^18
+  const taxRate = await terra.treasury.taxRate()
+  const taxCap = (await terra.treasury.taxCap(coin.denom)).amount
+  const amount = coin.amount
+  const tax = amount.sub(
+    amount
+      .mul(DECIMAL_FRACTION)
+      .div(DECIMAL_FRACTION.mul(taxRate).add(DECIMAL_FRACTION))
+  )
+  return tax.gt(taxCap) ? taxCap : tax
+}
+
+async function deductTax(terra: LCDClient, coin: Coin) {
+  return coin.amount.sub(await computeTax(terra, coin)).floor()
+}
+
+// TYPES
+
+interface Native { native: { denom: string } }
+
+interface CW20 { cw20: { contract_addr: string } }
+
+type Asset = Native | CW20
+
+// MAIN
+
+async function main() {
+  // SETUP
+
+  setTimeoutDuration(0)
+
+  const terra = new LocalTerra()
+
+  // addresses
+  const deployer = terra.wallets.test1
+  const alice = terra.wallets.test2
+  const bob = terra.wallets.test3
+  // mock contract addresses
+  const staking = terra.wallets.test8.key.accAddress
+  const safetyFund = terra.wallets.test9.key.accAddress
+  const treasury = terra.wallets.test10.key.accAddress
+
+  console.log("upload contracts")
+
+  const addressProvider = await deployContract(terra, deployer, "../artifacts/address_provider.wasm",
+    { owner: deployer.key.accAddress }
+  )
+
+  const incentives = await deployContract(terra, deployer, "../artifacts/incentives.wasm",
+    {
+      owner: deployer.key.accAddress,
+      address_provider_address: addressProvider
+    }
+  )
+
+  const oracle = await deployContract(terra, deployer, "../artifacts/oracle.wasm",
+    { owner: deployer.key.accAddress }
+  )
+
+  const maTokenCodeId = await uploadContract(terra, deployer, "../artifacts/ma_token.wasm")
+
+  const redBank = await deployContract(terra, deployer, "../artifacts/red_bank.wasm",
+    {
+      config: {
+        owner: deployer.key.accAddress,
+        address_provider_address: addressProvider,
+        ma_token_code_id: maTokenCodeId,
+        close_factor: "0.5",
+      }
+    }
+  )
+
+  const tokenCodeID = await uploadContract(terra, deployer, join(TERRASWAP_ARTIFACTS_PATH, "terraswap_token.wasm"))
+  const pairCodeID = await uploadContract(terra, deployer, join(TERRASWAP_ARTIFACTS_PATH, "terraswap_pair.wasm"))
+  const terraswapFactory = await deployContract(terra, deployer, join(TERRASWAP_ARTIFACTS_PATH, "terraswap_factory.wasm"),
+    {
+      pair_code_id: pairCodeID,
+      token_code_id: tokenCodeID
+    }
+  )
+
+  const protocolRewardsCollector = await deployContract(terra, deployer, "../artifacts/protocol_rewards_collector.wasm",
+    {
+      config: {
+        owner: deployer.key.accAddress,
+        address_provider_address: addressProvider,
+        safety_fund_fee_share: String(SAFETY_FUND_FEE_SHARE),
+        treasury_fee_share: String(TREASURY_FEE_SHARE),
+        astroport_factory_address: terraswapFactory,
+        astroport_max_spread: "0.05",
+      }
+    }
+  )
+
+  // update address provider
+  console.log("update address provider")
+  await executeContract(terra, deployer, addressProvider,
+    {
+      update_config: {
+        config: {
+          owner: deployer.key.accAddress,
+          // mars_token_address: mars,
+          protocol_rewards_collector_address: protocolRewardsCollector,
+          staking_address: staking,
+          treasury_address: treasury,
+          insurance_fund_address: safetyFund,
+          incentives_address: incentives,
+          oracle_address: oracle,
+          red_bank_address: redBank,
+          protocol_admin_address: deployer.key.accAddress,
+        }
+      }
+    }
+  )
+
+  console.log("init assets")
+
+  // uluna
+  await executeContract(terra, deployer, redBank,
+    {
+      init_asset: {
+        asset: { native: { denom: "uluna" } },
+        asset_params: {
+          initial_borrow_rate: "0.1",
+          max_loan_to_value: String(MAX_LTV),
+          reserve_factor: "0.2",
+          maintenance_margin: String(MAX_LTV + 0.001),
+          liquidation_bonus: String(LIQUIDATION_BONUS),
+          interest_rate_strategy: {
+            linear: {
+              optimal_utilization_rate: "0.1", // TODO panics with 0
+              base: String(INTEREST_RATE),
+              slope_1: "0",
+              slope_2: "0",
+            }
+          },
+          active: true,
+          deposit_enabled: true,
+          borrow_enabled: true
+        }
+      }
+    }
+  )
+  await setAssetOraclePriceSource(terra, deployer, oracle,
+    { native: { denom: "uluna" } },
+    LUNA_USD_PRICE
+  )
+  const maUluna = await queryMaAssetAddress(terra, redBank, { native: { denom: "uluna" } })
+
+  // uusd
+  await executeContract(terra, deployer, redBank,
+    {
+      init_asset: {
+        asset: { native: { denom: "uusd" } },
+        asset_params: {
+          initial_borrow_rate: "0.2",
+          max_loan_to_value: "0.75",
+          reserve_factor: "0.2",
+          maintenance_margin: "0.85",
+          liquidation_bonus: String(LIQUIDATION_BONUS),
+          interest_rate_strategy: {
+            linear: {
+              optimal_utilization_rate: "0.1",
+              base: String(INTEREST_RATE),
+              slope_1: "0",
+              slope_2: "0",
+            }
+          },
+          active: true,
+          deposit_enabled: true,
+          borrow_enabled: true
+        }
+      }
+    }
+  )
+  await setAssetOraclePriceSource(terra, deployer, oracle,
+    { native: { denom: "uusd" } },
+    1
+  )
+  const maUusd = await queryMaAssetAddress(terra, redBank, { native: { denom: "uusd" } })
+
+  // terraswap pairs
+
+  // let result = await executeContract(terra, deployer, terraswapFactory,
+  //   {
+  //     create_pair: {
+  //       asset_infos: [
+  //         { token: { contract_addr: mars } },
+  //         { native_token: { denom: "uusd" } }
+  //       ]
+  //     }
+  //   }
+  // )
+  // const marsUusdPair = result.logs[0].eventsByType.wasm.pair_contract_addr[0]
+
+  // result = await executeContract(terra, deployer, terraswapFactory,
+  //   {
+  //     create_pair: {
+  //       asset_infos: [
+  //         { native_token: { denom: "uluna" } },
+  //         { native_token: { denom: "uusd" } }
+  //       ]
+  //     }
+  //   }
+  // )
+  // const ulunaUusdPair = result.logs[0].eventsByType.wasm.pair_contract_addr[0]
+
+  // await executeContract(terra, deployer, ulunaUusdPair,
+  //   {
+  //     provide_liquidity: {
+  //       assets: [
+  //         {
+  //           info: { native_token: { denom: "uluna" } },
+  //           amount: String(ULUNA_UUSD_PAIR_ULUNA_LP_AMOUNT)
+  //         }, {
+  //           info: { native_token: { denom: "uusd" } },
+  //           amount: String(ULUNA_UUSD_PAIR_UUSD_LP_AMOUNT)
+  //         }
+  //       ]
+  //     }
+  //   },
+  //   `${ULUNA_UUSD_PAIR_ULUNA_LP_AMOUNT}uluna,${ULUNA_UUSD_PAIR_UUSD_LP_AMOUNT}uusd`,
+  // )
+
+  // await executeContract(terra, deployer, mars,
+  //   {
+  //     mint: {
+  //       recipient: deployer.key.accAddress,
+  //       amount: String(MARS_UUSD_PAIR_MARS_LP_AMOUNT)
+  //     }
+  //   }
+  // )
+
+  // await executeContract(terra, deployer, mars,
+  //   {
+  //     increase_allowance: {
+  //       spender: marsUusdPair,
+  //       amount: String(MARS_UUSD_PAIR_MARS_LP_AMOUNT),
+  //     }
+  //   }
+  // )
+
+  // await executeContract(terra, deployer, marsUusdPair,
+  //   {
+  //     provide_liquidity: {
+  //       assets: [
+  //         {
+  //           info: { token: { contract_addr: mars } },
+  //           amount: String(MARS_UUSD_PAIR_MARS_LP_AMOUNT)
+  //         }, {
+  //           info: { native_token: { denom: "uusd" } },
+  //           amount: String(MARS_UUSD_PAIR_UUSD_LP_AMOUNT)
+  //         }
+  //       ]
+  //     }
+  //   },
+  //   `${MARS_UUSD_PAIR_UUSD_LP_AMOUNT}uusd`,
+  // )
+
+  // TESTS
+
+  console.log("tests")
+
+  const provider = alice
+  const borrower = bob
+
+  console.log("provider provides uusd")
+
+  let maUusdBalanceBefore = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+  strictEqual(maUusdBalanceBefore, 0)
+
+  await depositNative(terra, provider, redBank, "uusd", USD_COLLATERAL_AMOUNT)
+
+  let maUusdBalanceAfter = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+  strictEqual(maUusdBalanceAfter, 0)
+
+  console.log("borrower provides uluna")
+
+  await depositNative(terra, borrower, redBank, "uluna", LUNA_COLLATERAL_AMOUNT)
+
+  console.log("borrower borrows uusd up to the borrow limit of their uluna collateral")
+
+  await borrowNative(terra, borrower, redBank, "uusd", Math.floor(USD_BORROW_AMOUNT))
+
+  console.log("repay")
+
+  maUusdBalanceBefore = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+
+  await executeContract(terra, borrower, redBank,
+    { repay_native: { denom: "uusd" } },
+    `${Math.floor(USD_BORROW_AMOUNT)}uusd`
+  )
+
+  maUusdBalanceAfter = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+  assert(maUusdBalanceAfter > maUusdBalanceBefore)
+
+  console.log("withdraw")
+
+  maUusdBalanceBefore = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+
+  await executeContract(terra, provider, redBank,
+    {
+      withdraw: {
+        asset: { native: { denom: "uusd" } },
+        amount: String(Math.floor(USD_COLLATERAL_AMOUNT / 2))
+      }
+    }
+  )
+
+  maUusdBalanceAfter = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+  assert(maUusdBalanceAfter > maUusdBalanceBefore)
+
+  console.log("protocol rewards collector withdraws from the red bank")
+
+  console.log("- specify an amount")
+
+  maUusdBalanceBefore = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+  let uusdBalanceBefore = await queryNativeBalance(terra, protocolRewardsCollector, "uusd")
+
+  // withdraw half
+  await executeContract(terra, deployer, protocolRewardsCollector,
+    {
+      withdraw_from_red_bank: {
+        asset: { native: { denom: "uusd" } },
+        amount: String(Math.floor(maUusdBalanceBefore / MA_TOKEN_SCALING_FACTOR / 2))
+      }
+    }
+  )
+
+  maUusdBalanceAfter = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+  let uusdBalanceAfter = await queryNativeBalance(terra, protocolRewardsCollector, "uusd")
+  assert(maUusdBalanceAfter < maUusdBalanceBefore)
+  assert(uusdBalanceAfter > uusdBalanceBefore)
+
+  console.log("- don't specify an amount")
+
+  maUusdBalanceBefore = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+  uusdBalanceBefore = await queryNativeBalance(terra, protocolRewardsCollector, "uusd")
+
+  // withdraw remaining balance
+  let result = await executeContract(terra, deployer, protocolRewardsCollector,
+    { withdraw_from_red_bank: { asset: { native: { denom: "uusd" } } } }
+  )
+
+  maUusdBalanceAfter = await queryCw20Balance(terra, protocolRewardsCollector, maUusd)
+  uusdBalanceAfter = await queryNativeBalance(terra, protocolRewardsCollector, "uusd")
+
+  // withdrawing from the red bank triggers protocol rewards to be minted to the protocol rewards
+  // collector, so the maUusd balance will not be zero after this call
+  let maUusdMintAmount = parseInt(result.logs[0].eventsByType.wasm.amount[0])
+  strictEqual(maUusdBalanceAfter, maUusdMintAmount)
+
+  assert(uusdBalanceAfter > uusdBalanceBefore)
+
+  console.log("try to distribute uusd rewards")
+
+  let protocolRewardsCollectorUusdBalance = await queryNativeBalance(terra, protocolRewardsCollector, "uusd")
+
+  await assert.rejects(
+    executeContract(terra, deployer, protocolRewardsCollector,
+      { distribute_protocol_rewards: { asset: { native: { denom: "uusd" } } } }
+    ),
+    (error: any) => {
+      return error.response.data.error.includes("Asset is not enabled for distribution: \"uusd\"")
+    }
+  )
+
+  console.log("enable uusd for distribution")
+
+  await executeContract(terra, deployer, protocolRewardsCollector,
+    {
+      update_asset_config: {
+        asset: { native: { denom: "uusd" } },
+        enabled: true
+      }
+    }
+  )
+
+  console.log("distribute uusd rewards")
+
+  let protocolRewardsCollectorUusdBalanceBefore = await queryNativeBalance(terra, protocolRewardsCollector, "uusd")
+  let treasuryUusdBalanceBefore = await queryNativeBalance(terra, treasury, "uusd")
+  let safetyFundUusdBalanceBefore = await queryNativeBalance(terra, safetyFund, "uusd")
+  let stakingUusdBalanceBefore = await queryNativeBalance(terra, staking, "uusd")
+
+  result = await executeContract(terra, deployer, protocolRewardsCollector,
+    { distribute_protocol_rewards: { asset: { native: { denom: "uusd" } } } }
+  )
+
+  let protocolRewardsCollectorUusdBalanceAfter = await queryNativeBalance(terra, protocolRewardsCollector, "uusd")
+  let treasuryUusdBalanceAfter = await queryNativeBalance(terra, treasury, "uusd")
+  let safetyFundUusdBalanceAfter = await queryNativeBalance(terra, safetyFund, "uusd")
+  let stakingUusdBalanceAfter = await queryNativeBalance(terra, staking, "uusd")
+
+  console.log(result.logs[0].eventsByType)
+  // console.log(protocolRewardsCollectorUusdBalanceBefore)
+  // console.log(protocolRewardsCollectorUusdBalanceAfter)
+
+  // TODO why is `protocolRewardsCollectorUusdBalanceAfter == 3`? rounding errors from integer arithmetic?
+  try {
+    strictEqual(protocolRewardsCollectorUusdBalanceAfter, 0)
+  } catch (error) {
+    console.log(error)
+  }
+
+  const protocolRewardsCollectorUusdBalanceDifference =
+    protocolRewardsCollectorUusdBalanceBefore - protocolRewardsCollectorUusdBalanceAfter
+
+  const expectedTreasuryUusdBalanceDifference =
+    (await deductTax(
+      terra,
+      new Coin("uusd", protocolRewardsCollectorUusdBalanceDifference * TREASURY_FEE_SHARE)
+    )).toNumber()
+  const expectedSafetyFundUusdBalanceDifference =
+    (await deductTax(
+      terra,
+      new Coin("uusd", protocolRewardsCollectorUusdBalanceDifference * SAFETY_FUND_FEE_SHARE)
+    )).toNumber()
+
+  const expectedStakingUusdBalanceDifference =
+    (await deductTax(
+      terra,
+      new Coin("uusd", protocolRewardsCollectorUusdBalanceDifference -
+        (expectedTreasuryUusdBalanceDifference + expectedSafetyFundUusdBalanceDifference))
+    )).toNumber()
+
+  try {
+    strictEqual(
+      treasuryUusdBalanceAfter - treasuryUusdBalanceBefore,
+      expectedTreasuryUusdBalanceDifference
+    )
+  } catch (error) {
+    console.log(error)
+  }
+  try {
+    strictEqual(
+      safetyFundUusdBalanceAfter - safetyFundUusdBalanceBefore,
+      expectedSafetyFundUusdBalanceDifference
+    )
+  } catch (error) {
+    console.log(error)
+  }
+  try {
+    strictEqual(
+      stakingUusdBalanceAfter - stakingUusdBalanceBefore,
+      expectedStakingUusdBalanceDifference
+    )
+  } catch (error) {
+    console.log(error)
+  }
+
+
+  const tmp =
+    (await deductTax(
+      terra,
+      new Coin("uusd", protocolRewardsCollectorUusdBalanceDifference * 0.7)
+    )).toNumber()
+
+  try {
+    strictEqual(expectedStakingUusdBalanceDifference, tmp)
+  } catch (error) {
+    console.log(error)
+  }
+
+
+  console.log("OK")
+}
+
+main().catch(err => console.log(err))
