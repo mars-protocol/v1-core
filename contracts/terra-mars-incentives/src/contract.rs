60a7a1731218100e842a40a35e62b2f5e86f5252
Spike Spiegel <spikeonmars@protonmail.com>
2021-09-27 15:51:12 -0300

    Renaming wip

--- contracts/terra-mars-incentives/src/contract.rs
+++ contracts/terra-mars-incentives/src/contract.rs
@@ -250,7 +250,7 @@ pub fn execute_claim_rewards(
             msg: to_binary(&cw20::Cw20ExecuteMsg::Send {
                 contract: staking_address.to_string(),
                 amount: total_unclaimed_rewards,
-                msg: to_binary(&staking::msg::ReceiveMsg::Stake {
+                msg: to_binary(&terra_mars::staking::msg::ReceiveMsg::Stake {
                     recipient: Some(user_address.to_string()),
                 })?,
             })?,
