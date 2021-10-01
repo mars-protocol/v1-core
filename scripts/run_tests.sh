3bb6e42eca7a9877530dc350050374291ffd0687
harryscholes <harryscholes@users.noreply.github.com>
2021-10-01 13:30:22 +0100

    Update script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,19 +1,34 @@
-# docker compose -f $LOCAL_TERRA_PATH/docker-compose.yml up -d
+#!/bin/bash
 
-for test in tests/liquidations*.ts; do
-  # oracle test must be run with slower block times
-  if [[ $test =~ oracle.ts ]]; then
-    continue
-  fi
+# Instructions:
+# LOCAL_TERRA_REPO_PATH must be provided
 
+set -e
+
+docker compose -f $LOCAL_TERRA_REPO_PATH/docker-compose.yml down
+
+sed -E -i .bak '/timeout_(propose|prevote|precommit|commit)/s/[0-9]+m?s/200ms/' $LOCAL_TERRA_REPO_PATH/config/config.toml
+
+docker compose -f $LOCAL_TERRA_REPO_PATH/docker-compose.yml up -d
+
+# oracle tests must be run with slower block times
+tests=$(ls tests/*.ts | grep -v oracle)
+
+for test in $tests; do
   echo Running $test
   node --loader ts-node/esm $test
-
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-    echo Error $test
-    return $exit_code
-  fi
 done
 
-# docker compose -f $LOCAL_TERRA_PATH/docker-compose.yml down
+docker compose -f $LOCAL_TERRA_REPO_PATH/docker-compose.yml down
+
+# oracle tests
+sed -E -i .bak '/timeout_(propose|prevote|precommit|commit)/s/[0-9]+m?s/1500ms/' $LOCAL_TERRA_REPO_PATH/config/config.toml
+
+docker compose -f $LOCAL_TERRA_REPO_PATH/docker-compose.yml up -d
+
+echo Running tests/oracle.ts
+node --loader ts-node/esm tests/oracle.ts
+
+docker compose -f $LOCAL_TERRA_REPO_PATH/docker-compose.yml down
+
+sed -E -i .bak '/timeout_(propose|prevote|precommit|commit)/s/[0-9]+m?s/200ms/' $LOCAL_TERRA_REPO_PATH/config/config.toml
