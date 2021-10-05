89ca8630a9631457ac9e2863a00c42ccd0afcd8a
harryscholes <harryscholes@users.noreply.github.com>
2021-10-05 10:56:28 +0100

    Update create_unsigned_tx.ts

--- scripts/multisig/create_unsigned_tx.ts
+++ scripts/multisig/create_unsigned_tx.ts
@@ -5,26 +5,21 @@ import 'dotenv/config.js'
 import { createTransaction } from "../helpers.js"
 
 // Required environment variables:
-
-// Terra network details
+// Terra network details:
 const CHAIN_ID = process.env.CHAIN_ID!
 const LCD_CLIENT_URL = process.env.LCD_CLIENT_URL!
-
-// Multisig details
-// The address that the tx will be sent from
-const MULTISIG_ADDRESS = process.env.MULTISIG_ADDRESS!
+// Multisig details:
 // The name of the multisig account in terracli
 const MULTISIG_NAME = process.env.MULTISIG_NAME!
-
-// Transaction details
+// Transaction details:
 // The address that the tx will be sent to
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS!
 // A JSON object of the operation to be executed on the contract
-const EXECUTE_MSG = JSON.parse(process.env.EXECUTE_MSG!)
+const EXECUTE_MSG = JSON.parse(process.env.EXECUTE_MSG!);
 
-// Main
+// MAIN
 
-async function main() {
+(async () => {
   const terra = new LCDClient({
     URL: LCD_CLIENT_URL,
     chainID: CHAIN_ID
@@ -33,32 +28,34 @@ async function main() {
   // Create an unsigned tx
   const multisig = new Wallet(terra, new CLIKey({ keyName: MULTISIG_NAME }))
 
-  const msg = new MsgExecuteContract(MULTISIG_ADDRESS, CONTRACT_ADDRESS, EXECUTE_MSG)
+  const multisigAddress = multisig.key.accAddress
 
-  const tx = await createTransaction(terra, multisig, msg)
+  const tx = await createTransaction(multisig,
+    new MsgExecuteContract(multisigAddress, CONTRACT_ADDRESS, EXECUTE_MSG)
+  )
+
+  const accInfo = await terra.auth.accountInfo(multisigAddress)
 
   // The unsigned tx file should be distributed to the multisig key holders
   const unsignedTx = "unsigned_tx.json"
-  writeFileSync(unsignedTx, tx.toStdTx().toJSON())
+  writeFileSync(unsignedTx, JSON.stringify(tx.toData()))
 
   // Prints a command that should be run by the multisig key holders to generate signatures
   // TODO add Ledger support
   console.log(`
-# Set \`from\` to your address that is a key to the multisig: ${MULTISIG_ADDRESS}
+# Set \`from\` to your address that is a key to the multisig: ${multisigAddress}
 
 from=terra1...
 
-terracli tx sign ${unsignedTx} \\
-  --multisig ${MULTISIG_ADDRESS} \\
+terrad tx sign ${unsignedTx} \\
+  --multisig ${multisigAddress} \\
   --from \$from \\
-  --chain-id ${tx.chain_id} \\
+  --chain-id ${terra.config.chainID} \\
   --offline \\
-  --account-number ${tx.account_number} \\
-  --sequence ${tx.sequence} \\
+  --account-number ${accInfo.getAccountNumber()} \\
+  --sequence ${accInfo.getSequenceNumber()} \\
   --output-document \${from}_sig.json
 `)
 
   // Run `broadcast_tx.ts` to aggregate at least K of N signatures and broadcast the signed tx to the network
-}
-
-main().catch(err => console.log(err))
+})()
