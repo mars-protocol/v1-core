5dbf51606791fc8ad9d723304be6518a45facb62
harryscholes <harryscholes@users.noreply.github.com>
2021-11-02 16:55:02 +0000

    Fix test

--- scripts/tests/oracle_spot_and_twap.ts
+++ scripts/tests/oracle_spot_and_twap.ts
@@ -71,7 +71,7 @@ async function recordTwapSnapshots() {
     },
   });
   const timestamp = parseInt(result.logs[0].eventsByType.from_contract.timestamp[0]);
-  const cumulativePrice = parseInt(result.logs[0].eventsByType["wasm-record-twap-snapshot"].price_cumulative[0]);
+  const cumulativePrice = parseInt(result.logs[0].eventsByType.wasm.price_cumulative[0]);
   return { timestamp, cumulativePrice };
 }
 
