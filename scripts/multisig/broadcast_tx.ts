e4a791643c6c0f69d15a7991abc69a1ee358dfd2
Harry Scholes <harryscholes@users.noreply.github.com>
2021-12-01 16:13:24 +0200

    Remove dependency on helpers.ts

--- scripts/multisig/broadcast_tx.ts
+++ scripts/multisig/broadcast_tx.ts
@@ -9,7 +9,6 @@ import { SignatureV2 } from "@terra-money/terra.js/dist/core/SignatureV2.js"
 import { MultiSignature } from "@terra-money/terra.js/dist/core/MultiSignature.js"
 import { readFileSync } from "fs"
 import 'dotenv/config.js'
-import { transactionErrorFromResult } from "../helpers.js"
 
 // Required environment variables:
 // Terra network details:
@@ -72,7 +71,7 @@ const SIGNATURES = (process.env.SIGNATURES!).split(",");
   // Broadcast the tx
   const result = await terra.tx.broadcast(tx)
   if (isTxError(result)) {
-    throw transactionErrorFromResult(result)
+    throw new Error(result.raw_log)
   }
   console.log(`https://finder.terra.money/${CHAIN_ID}/tx/${result.txhash}`)
 })()
