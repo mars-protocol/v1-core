63f5423f7a5807bc2ca31ce45f8b79d74c315e01
Harry Scholes <harryscholes@users.noreply.github.com>
2021-12-02 14:22:40 +0200

    Update scripts

--- scripts/multisig/create_unsigned_tx.ts
+++ scripts/multisig/create_unsigned_tx.ts
@@ -9,12 +9,22 @@ import 'dotenv/config.js'
 
 // CONSTS
 
-// Required environment variables:
+if (!(process.env.CHAIN_ID
+  && process.env.LCD_CLIENT_URL
+  && process.env.MULTISIG_PUBLIC_KEYS
+  && process.env.MULTISIG_THRESHOLD
+  && process.env.CONTRACT_ADDRESS
+  && process.env.TRANSACTION_DESCRIPTION
+  && process.env.EXECUTE_MSG
+)) {
+  throw new Error("One or more required environment variables are missing")
+}
+
 // Terra network details:
-const CHAIN_ID = process.env.CHAIN_ID!
-const LCD_CLIENT_URL = process.env.LCD_CLIENT_URL!
+const CHAIN_ID = process.env.CHAIN_ID
+const LCD_CLIENT_URL = process.env.LCD_CLIENT_URL
 // Multisig details:
-const MULTISIG_PUBLIC_KEYS = (process.env.MULTISIG_PUBLIC_KEYS!)
+const MULTISIG_PUBLIC_KEYS = process.env.MULTISIG_PUBLIC_KEYS
   .split(",")
   // terrad sorts keys of multisigs by comparing bytes of their address
   .sort((a, b) => {
@@ -27,14 +37,14 @@ const MULTISIG_PUBLIC_KEYS = (process.env.MULTISIG_PUBLIC_KEYS!)
     )
   })
   .map(x => new SimplePublicKey(x))
-const MULTISIG_THRESHOLD = parseInt(process.env.MULTISIG_THRESHOLD!)
+const MULTISIG_THRESHOLD = parseInt(process.env.MULTISIG_THRESHOLD)
 // Transaction details:
 // The address that the tx will be sent to
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS!
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS
 // A description of the transaction
-const TRANSACTION_DESCRIPTION = process.env.TRANSACTION_DESCRIPTION!
+const TRANSACTION_DESCRIPTION = process.env.TRANSACTION_DESCRIPTION
 // A JSON object of the operation to be executed on the contract
-const EXECUTE_MSG = JSON.parse(process.env.EXECUTE_MSG!);
+const EXECUTE_MSG = JSON.parse(process.env.EXECUTE_MSG);
 
 // MAIN
 
@@ -78,39 +88,30 @@ const EXECUTE_MSG = JSON.parse(process.env.EXECUTE_MSG!);
 
   // Prints a command that should be run by the multisig key holders to generate signatures
   console.log(`
-Instructions to sign ${TRANSACTION_DESCRIPTION}:
+Multisig transaction signing instructions
 
-- Set \`from\` to your address that is a key to the multisig ${multisigAddress}:
+Multisig: ${multisigAddress}
+Transaction: ${TRANSACTION_DESCRIPTION}
+
+- Set \`signing_address\` to your address that you are signing the transaction with
+- Set \`first_name\` to your first name
+- If your address is on a Ledger hardware device, make sure the Terra app is open
+- Run the signing command
+- Return the signed json file
 
 \`\`\`
-from=terra1...
-\`\`\`
+signing_address=terra1...
 
-- Set \`first_name\` to your first name:
-
-\`\`\`
 first_name=...
-\`\`\`
 
-- Run the signing command:
-
-\`\`\`
 terrad tx sign ${unsignedTxFilename} \\
   --multisig ${multisigAddress} \\
-  --from \$from \\
+  --from \$signing_address \\
   --chain-id ${terra.config.chainID} \\
   --offline \\
   --account-number ${accInfo.getAccountNumber()} \\
   --sequence ${accInfo.getSequenceNumber()} \\
   --output-document ${TRANSACTION_DESCRIPTION}_signed_\${first_name}.json
-\`\`\`
-
-Optionally add the \`--ledger\` flag if your address's private key is on a
-Ledger hardware device. The Terra app must be open on your Ledger when you run
-the command.
-
-\`\`\`
-terrad tx sign ... --ledger
 \`\`\`
 `)
 
